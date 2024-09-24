2f2f4bdee8a05a0829f27a1b5d5b7d158b744fac
SymuNadeem <msymunnadeem@gmail.com>
2024-09-24 23:23:54 +0600

    binary search ori

--- Search_Algorithm/binarySearch.js
+++ Search_Algorithm/binarySearch.js
@@ -2,7 +2,6 @@
 
 // arr=[-5,8,7,9,1],t=9 --> should return 3
 
-
 function binarySearch(arr, t) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === t) {
@@ -12,3 +11,27 @@ function binarySearch(arr, t) {
   return -1;
 }
 console.log(binarySearch([-5, 10, 5, 8, 7, 5], 7));
+
+// Original binarySearch::
+
+// function binaryOriginalSearch(arr, target) {
+//   let leftIndex = 0;
+//   let rightIndex = arr.length - 1;
+//   while (leftIndex <= rightIndex) {
+//     let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+//     if (target === arr[middleIndex]) {
+//       return middleIndex;
+//     }
+//     if (target < arr[middleIndex]) {
+//       rightIndex = middleIndex - 1;
+//     } else {
+//       leftIndex = middleIndex + 1;
+//     }
+//   }
+//   return -1;
+// }
+
+// console.log(binaryOriginalSearch([-5, 8, 9, 78, 10, 6], 78));
+// console.log(binaryOriginalSearch([-15, 8, 9, 78, 10, 6], 10));
+// console.log(binaryOriginalSearch([20, -8, 10, 78, 10, 6], 6));
+// console.log(binaryOriginalSearch([-5, 8, 9, 78, 10, 65], 65));
