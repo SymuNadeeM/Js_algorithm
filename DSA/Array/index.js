4b21941b6353135afe91a0f098d3345ba50656e9
SymuNadeem <msymunnadeem@gmail.com>
2025-01-31 23:19:02 +0600

    DSA Array custom array

--- DSA/Array/index.js
+++ DSA/Array/index.js
@@ -1,3 +1,6 @@
+
+// ::Custom Array With Push::
+/* 
 class myArray{
   constructor(){
     this.length =0,
@@ -17,3 +20,28 @@ myNewArray.push("Z");
 
 console.log(myNewArray.data);
 
+*/
+
+class myArray{
+  constructor(){
+    this.length =0;
+    this.data = {}
+  }
+  push(item){
+    this.data[this.length] = item
+    this.length ++ ;
+    return this.length
+  }
+  get(index){
+    return this.data[index]
+  }
+  
+}
+
+const arr = new myArray()
+arr.push("Apple")
+arr.push("Orange")
+arr.push("Banana")
+arr.push("Watermill")
+
+console.log(arr.get(2));
